26156a48398196c5e71fa0438d4e48def6b7113b
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-05 00:55:50 +0200

    Adding the dashboard summaries

--- js/admin-home.js
+++ js/admin-home.js
@@ -13,20 +13,35 @@ const getTotalBlogs = async () => {
     console.error("Error fetching blogs:", error.message);
   }
 };
+const getTotalProjects = async () => {
+  try{
+  const response = await fetch(
+    "https://my-brand-backend-server.onrender.com/api/project"
+  );
+  const data = await response.json();
+  const projectsTotalSection = document.getElementById("projectsTotal");
 
-getTotalBlogs();
-
-
-
-
-
-const getTotalMessages = async()=> {
+  const dataArr = data.data;
+  projectsTotalSection.innerHTML = dataArr.length;
+} catch (error) {
+  console.error("Error fetching messages:", error.message);
+}
+};
+const getTotalMessages = async () => {
   try {
-    const response = await fetch("https://my-brand-backend-server.onrender.com/api/message");
-    const data= await response.json()
-    console.log(data)
+    const response = await fetch(
+      "https://my-brand-backend-server.onrender.com/api/message"
+    );
+    const data = await response.json();
+    const messagesTotalSection = document.getElementById("messagesTotal");
+
+    const dataArr = data.messagesData;
+    messagesTotalSection.innerHTML = dataArr.length;
   } catch (error) {
     console.error("Error fetching messages:", error.message);
-    
   }
-}
+};
+
+getTotalBlogs();
+getTotalProjects();
+getTotalMessages();
